ca5663ed8dc1c441c2de06e70b6c42c8925a6c3c
wintrmut3 <jamesx03@gmail.com>
2021-09-03 20:29:23 -0400

    added scoreboard / firebase link

---
+++ firebase.js
@@ -0,0 +1,8 @@
+// import { addDoc, collection, doc, getDocs, getFirestore, increment, setDoc, updateDoc, getDoc } from "firebase/firestore";
+
+const InitFirestore = async () => {
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+}
